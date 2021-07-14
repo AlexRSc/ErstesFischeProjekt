0026c3b8d31f9fef5836d4a55f1ada513741a986
Alexander Schaer <alexanderschaer@MacBook-Pro-von-Alexander.local>
2021-07-14 07:38:34 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        String firstWord= "pew";
+        String secWord="pow";
+        int numOne=2;
+        int numTwo=3;
+        boolean yes=true;
+        boolean no=false;
+        String combination= firstWord+numOne;
+        System.out.println(combination);
+    }
+}
---
+++ src/text.md
@@ -0,0 +1,16 @@
+#Title
+
+* sexypointers
+
+testytest
+
+`olala`
+ich teste diese neue Tastatur
+man kann drauf schnell schreiben
+
+FUCK YEAH!
+
+´testytest´
+##sectitle
+###triptitle
+
